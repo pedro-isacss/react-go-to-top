fcf15cbaaa3bdbfb0ddfa3670635c1885cef9139
Pedro Isac <ss.pedroisac@gmail.com>
2023-05-13 14:32:47 -0300

    Update index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,55 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import BackToTop from "./BackToTop";
+import React, { useState, useEffect } from "react";
+import styles from "./index.module.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <BackToTop />
-  </React.StrictMode>
-);
+function BackToTop({
+  icon,
+  backgroundColor,
+  bottom,
+  right,
+  size,
+  offsetToShow,
+}) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const onScroll = () => {
+    const offset = offsetToShow ? offsetToShow : 100;
+    if (window.pageYOffset > offset) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const onBackToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+  }, []);
+
+  return (
+    <>
+      {isVisible && (
+        <button
+          onClick={onBackToTop}
+          className={styles.backtotop}
+          style={{
+            backgroundColor: backgroundColor ? backgroundColor : "#000",
+            bottom: bottom ? bottom : 24,
+            right: right ? right : 24,
+            width: size ? size : 48,
+            height: size ? size : 48,
+          }}
+        >
+          {icon ? icon : <img src="top.svg" alt="Top" />}
+        </button>
+      )}
+    </>
+  );
+}
+
+export default BackToTop;
